395afbe4baa5686f84a58e1f1dc963e954472b5c
eliasalmarza20@gmail.com <eliasalmarza20@gmail.com>
2021-09-06 21:17:17 -0300

    add new branch

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,7 +1,7 @@
 package com.paymentchain.billing;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String meessage="probando pipeleane";
+		String meessage= "probando pipeleane";
 		Assert.assertNotNUll(meessage);
 	}
 
